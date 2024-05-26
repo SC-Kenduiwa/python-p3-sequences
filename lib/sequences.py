ead62db542b7d3a3fbc94c9791861740f35b3a31
SC-Kenduiwa <kenduiwashanice64@gmail.com>
2024-05-26 12:20:17 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    prev1 = 0
+    prev2 = 0
+    counting = 0
+    fibonacci = list()
+    while counting < length:
+        sequence = prev1 +prev2
+        fibonacci.append(sequence)
+        #print(fibonacci, sequence)
+        if(sequence == 0):
+            prev1 += 1
+            prev2 = prev2
+        else:
+            prev1 = fibonacci[len(fibonacci) -1]
+            prev2 = fibonacci[len(fibonacci) -2]
+        
+        counting += 1
+    print(fibonacci)
+    return(fibonacci)
+x = print_fibonacci(9)
